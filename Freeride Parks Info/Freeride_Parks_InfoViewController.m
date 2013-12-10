9908fce641b500cd3a9043701f7c19ef67cbab0f
Appes <davidkratochvil@David-Mac-mini.local>
2013-12-10 13:49:08 +0100

    vole

--- Freeride Parks Info/Freeride_Parks_InfoViewController.m
+++ Freeride Parks Info/Freeride_Parks_InfoViewController.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2013 Appes developers s.r.o. All rights reserved.
 //
 
+//vole
+
 #import "Freeride_Parks_InfoViewController.h"
 #import "SeznamCell.h"
 #import <CoreLocation/CoreLocation.h>
